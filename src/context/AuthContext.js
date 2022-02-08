b4f9ccf1809a3d9d1744d0d86695bd3967650aa6
dev-vicki <vignesh.r1999@gmail.com>
2022-02-08 13:32:22 +0530

    used context api

---
+++ src/context/AuthContext.js
@@ -0,0 +1,32 @@
+import {createContext, useReducer} from "react";
+import AuthReducer from "./AuthReducer";
+
+const INITIAL_STATE = {
+    user: {
+        _id:"6200a15fe23690e48980d01e",
+    username:"joel",
+    email:"joel@gmail.com",
+    profilePicture:"person/1.jpeg",
+    coverPicture:"",
+    isAdmin: false,
+    followers: [],
+    followings: [],
+    },
+    isFetching: false,
+    error: false,
+};
+
+export const AuthContext = createContext(INITIAL_STATE);
+
+export const AuthContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+    return(
+        <AuthContext.Provider
+        value={{user: state.user,
+        isFetching:state.isFetching,
+        error:state.error, dispatch}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
